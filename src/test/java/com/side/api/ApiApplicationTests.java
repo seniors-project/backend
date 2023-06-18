948f904c80e4d079022bc5d7db2b728eeb0b3daa
ybchar <uiurihappy@sju.ac.kr>
2023-06-18 14:54:57 +0900

    init

---
+++ src/test/java/com/side/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.side.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
