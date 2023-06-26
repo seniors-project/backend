fd9935fed8f9ec5b7aa60deef05e5d09846d1be9
ybchar <uiurihappy@sju.ac.kr>
2023-06-26 14:42:50 +0900

    feat: User and redis config

---
+++ src/main/java/com/side/api/users/Users.java
@@ -0,0 +1,45 @@
+package com.side.api.users;
+
+import com.side.api.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Users extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(columnDefinition = "varchar(255) COMMENT 'sns 고유 ID'", unique = true)
+	private String snsId;
+
+	@Column(columnDefinition = "varchar(50) COMMENT '이메일'")
+	private String email;
+
+	@Column(columnDefinition = "varchar(50) COMMENT '비밀번호'")
+	private String password;
+
+	@Column(columnDefinition = "varchar(30) COMMENT '닉네임'", unique = true)
+	private String nickname;
+
+	@Column(columnDefinition = "varchar(10) COMMENT '성별'")
+	private String gender;
+
+	@Column(columnDefinition = "varchar(50) COMMENT '휴대전화번호'")
+	private String phoneNumber;
+
+	public static Users initSnsUsers(
+			String snsId, String email, String nickname
+	) {
+		return Users.builder()
+				.snsId(snsId)
+				.email(email)
+				.nickname(nickname)
+				.build();
+	}
+}
