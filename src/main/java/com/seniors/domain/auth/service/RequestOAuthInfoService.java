7039b583e3480ca4f514fb9ec73fcad2c8db357e
ybchar <uiurihappy@sju.ac.kr>
2023-07-05 12:28:02 +0900

    feat: refactoring

--- src/main/java/com/seniors/domain/auth/service/RequestOAuthInfoService.java
+++ src/main/java/com/seniors/domain/auth/service/RequestOAuthInfoService.java
@@ -17,12 +17,20 @@ public class RequestOAuthInfoService {
 	private final Map<OAuthProvider, OAuthApiClient> clients = new HashMap<>();
 
 	public RequestOAuthInfoService(List<OAuthApiClient> clients) {
+		if (clients == null) {
+			throw new IllegalArgumentException("Clients cannot be null");
+		}
+
 		for (OAuthApiClient client : clients) {
 			this.clients.put(client.oAuthProvider(), client);
 		}
 	}
 
 	public OAuthInfoResponse request(OAuthLoginParams params) {
+		if (params == null || params.oAuthProvider() == null) {
+			throw new IllegalArgumentException("OAuth provider must be specified");
+		}
+
 		OAuthApiClient client = clients.get(params.oAuthProvider());
 		if (client == null) {
 			throw new IllegalArgumentException("No client found for the given OAuth provider");
