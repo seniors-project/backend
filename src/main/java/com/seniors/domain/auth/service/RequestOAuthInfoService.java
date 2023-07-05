a3fb68c880058cd10f13903937ef8001161ee758
ybchar <uiurihappy@sju.ac.kr>
2023-07-05 12:21:24 +0900

    feat: refactoring

--- src/main/java/com/seniors/domain/auth/service/RequestOAuthInfoService.java
+++ src/main/java/com/seniors/domain/auth/service/RequestOAuthInfoService.java
@@ -6,6 +6,7 @@ import com.seniors.domain.auth.common.OAuthLoginParams;
 import com.seniors.common.constant.OAuthProvider;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -13,16 +14,20 @@ import java.util.stream.Collectors;
 
 @Component
 public class RequestOAuthInfoService {
-	private final Map<OAuthProvider, OAuthApiClient> clients;
+	private final Map<OAuthProvider, OAuthApiClient> clients = new HashMap<>();
 
 	public RequestOAuthInfoService(List<OAuthApiClient> clients) {
-		this.clients = clients.stream().collect(
-				Collectors.toUnmodifiableMap(OAuthApiClient::oAuthProvider, Function.identity())
-		);
+		for (OAuthApiClient client : clients) {
+			this.clients.put(client.oAuthProvider(), client);
+		}
 	}
 
 	public OAuthInfoResponse request(OAuthLoginParams params) {
 		OAuthApiClient client = clients.get(params.oAuthProvider());
+		if (client == null) {
+			throw new IllegalArgumentException("No client found for the given OAuth provider");
+		}
+
 		String accessToken = client.requestAccessToken(params);
 		return client.requestOAuthInfo(accessToken);
 	}
--- src/test/java/com/seniors/domain/post/PostControllerTest.java
+++ src/test/java/com/seniors/domain/post/PostControllerTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -24,6 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @SpringBootTest
 @ActiveProfiles("dev")
+@DirtiesContext
 class PostControllerTest {
 
 	@Autowired
@@ -40,7 +42,6 @@ class PostControllerTest {
 		postRepository.deleteAll();
 	}
 
-	@Deprecated
 	@Test
 	@DisplayName("게시글 생성")
 	void postAdd() throws Exception {
