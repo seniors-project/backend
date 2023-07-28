c3fb224d9f5528f3779a3eee6c5499f40af183e2
ybchar <uiurihappy@sju.ac.kr>
2023-07-28 15:11:05 +0900

    Feat: Controller Swagger Auth, Users

--- src/main/java/com/seniors/domain/users/controller/UsersController.java
+++ src/main/java/com/seniors/domain/users/controller/UsersController.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "사용자", description = "사용자 API 명세서")
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -32,7 +34,7 @@ public class UsersController {
 	 * @param userDetails
 	 * @return ResponseEntity
 	 */
-	@Operation(summary = "유저 검증")
+	@Operation(summary = "사용자 검증")
 	@ApiResponse(responseCode = "200", description = "검증 성공",
 			content = @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseEntity.class)))
 	@ApiResponse(responseCode = "401", description = "검증 실패",
