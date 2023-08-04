467ba10c95f912b69ad37e2a7f7577f0e264a4ad
ybchar <uiurihappy@sju.ac.kr>
2023-08-04 15:13:18 +0900

    Feat: Notification findAllByUsersId

--- src/main/java/com/seniors/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/seniors/domain/notification/repository/NotificationRepository.java
@@ -3,9 +3,8 @@ package com.seniors.domain.notification.repository;
 import com.seniors.domain.notification.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Arrays;
 import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long>, NotificationRepositoryCustom{
-	List<Notification> findAllByReceiverId(Long userId);
+	List<Notification> findAllByUsersId(Long userId);
 }
