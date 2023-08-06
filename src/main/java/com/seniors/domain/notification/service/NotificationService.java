40dd73e510fa203bdbf0518e74d8a0c66e6c99b9
ybchar <uiurihappy@sju.ac.kr>
2023-08-06 17:58:41 +0900

    Fix: 메서드 분리

--- src/main/java/com/seniors/domain/notification/service/NotificationService.java
+++ src/main/java/com/seniors/domain/notification/service/NotificationService.java
@@ -47,19 +47,28 @@ public class NotificationService {
 		emitter.onCompletion(() -> emitterRepository.deleteById(id));
 		emitter.onTimeout(() -> emitterRepository.deleteById(id));
 
-		// 503 에러를 방지하기 위한 더미 이벤트 전송
-//		sendToClient(emitter, id, "{\"message\": \"EventStream Created. [userId=" + userId + "]\"," +
-//				" \"url\": \"/api/posts\", \"content\": \"시니어스 구독 userId " + userId + " \", \"id\": " + userId + ", \"isRead\": false, \"createdAt\": \"2023-08-02 15:15:40.648816\"}");
+		// To prevent 503 error, send a dummy event
+//		sendDummyEvent(emitter, userId);
 
-		// 클라이언트가 미수신한 Event 목록이 존재할 경우 전송하여 Event 유실을 예방
+		// Send unsent Event list to prevent Event loss
+		sendUnsentEvents(emitter, userId, lastEventId);
+
+		return emitter;
+	}
+
+	private void sendDummyEvent(SseEmitter emitter, Long userId) {
+		String dummyEvent = "{\"message\": \"EventStream Created. [userId=" + userId + "]\"," +
+				" \"url\": \"/api/posts\", \"content\": \"시니어스 구독 userId " + userId + " \", \"id\": " + userId + ", \"isRead\": false, \"createdAt\": \"2023-01-01 01:01:01.123456\"}";
+		sendToClient(emitter, userId.toString(), dummyEvent);
+	}
+
+	private void sendUnsentEvents(SseEmitter emitter, Long userId, String lastEventId) {
 		if (lastEventId != null && !lastEventId.isEmpty()) {
 			Map<String, Object> events = emitterRepository.findAllEventCacheStartWithId(String.valueOf(userId));
 			events.entrySet().stream()
 					.filter(entry -> lastEventId.compareTo(entry.getKey()) < 0)
 					.forEach(entry -> sendToClient(emitter, entry.getKey(), entry.getValue()));
 		}
-
-		return emitter;
 	}
 
 	@Transactional
