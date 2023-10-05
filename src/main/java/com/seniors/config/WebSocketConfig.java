b1ddfde67ffae2563353231742883727372f494c
regchoi <tjrchldyd@gmail.com>
2023-10-05 17:49:16 +0900

    Refactor: 웹소켓 endpoint 변경

--- src/main/java/com/seniors/config/WebSocketConfig.java
+++ src/main/java/com/seniors/config/WebSocketConfig.java
@@ -1,18 +1,21 @@
 package com.seniors.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/api")
+		String[] endPoint = {"/api/chat", "/api/notification"};
+		registry.addEndpoint(endPoint)
 				.setAllowedOrigins("*");
 //				.withSockJS();
 	}
