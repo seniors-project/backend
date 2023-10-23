a26644b5c7169525972e1829715a1129fd53a2ca
regchoi <tjrchldyd@gmail.com>
2023-10-23 21:50:04 +0900

    Refactor : 사용하지 않는 코드 정리 #107

--- src/main/java/com/seniors/domain/chat/repository/ChatRoomMembersRepositoryImpl.java
+++ src/main/java/com/seniors/domain/chat/repository/ChatRoomMembersRepositoryImpl.java
@@ -1,30 +1,15 @@
 package com.seniors.domain.chat.repository;
 
-import com.querydsl.core.types.Expression;
-import com.querydsl.core.types.Predicate;
-import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.jpa.JPAExpressions;
-import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.seniors.common.repository.BasicRepoSupport;
-import com.seniors.domain.chat.entity.QChatMessage;
-import com.seniors.domain.chat.entity.QChatRoom;
 import com.seniors.domain.chat.entity.QChatRoomMembers;
-import com.seniors.domain.users.entity.QUsers;
 import jakarta.persistence.EntityManager;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
 
 @Slf4j
 public class ChatRoomMembersRepositoryImpl extends BasicRepoSupport implements ChatRoomMembersRepositoryCustom {
-
-	private final static QChatRoom chatRoom = QChatRoom.chatRoom;
-	private final static QUsers users = QUsers.users;
-	private final static QChatMessage chatMessage = QChatMessage.chatMessage;
 	private final static QChatRoomMembers chatRoomMembers = QChatRoomMembers.chatRoomMembers;
 
 	protected ChatRoomMembersRepositoryImpl(JPAQueryFactory jpaQueryFactory, EntityManager em) {
